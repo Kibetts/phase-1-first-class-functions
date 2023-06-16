54ecbd75bb38920db607f0093e72cadaa406c474
Brian Kibet <kibetsbrian@gmail.com>
2023-06-16 19:38:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback){
+   callback()
+
+};
+
+function returnsANamedFunction(){
+    return function anotherFunction(){
+
+    };
+
+};
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
